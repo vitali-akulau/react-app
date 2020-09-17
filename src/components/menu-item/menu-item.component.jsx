3e41c1bc751d7423dec3a04f6fc4e6f41fbba82a
s34rching <ivan.katsenya@gmail.com>
2020-09-17 23:03:49 +0300

    Add menu-item background transition

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,11 +6,11 @@ const MenuItem = ({ title, id, imageUrl, size }) => {
   const subtitleAction = 'shop now'
 
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`${size} menu-item`}
-      key={id}
-    >
+    <div className={`${size} menu-item`} key={id}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className='content'>
         <h1 className='title'>{_.toUpper(title)}</h1>
         <span className='subtitle'>{_.toUpper(subtitleAction)}</span>
