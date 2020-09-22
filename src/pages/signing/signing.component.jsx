22e42e67e93298bfeb600f586f0defd73e8eb455
s34rching <ivan.katsenya@gmail.com>
2020-09-22 19:47:41 +0300

    Add sign-in component

--- src/pages/signing/signing.component.jsx
+++ src/pages/signing/signing.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./signing.styles.scss"
+import SignIn from "../../components/sign-in/sign-in.component";
 
 const SigningPage = () => {
   return (
     <div>
       <h2>SIGN PAGE</h2>
+      <SignIn />
     </div>
   )
 }
