c6ecdf1f1109563973ada6d4d8b5a224226a746a
s34rching <ivan.katsenya@gmail.com>
2020-09-22 19:30:19 +0300

    Add signing page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
+
+import Header from "./components/header/header.component";
+
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import SigningPage from "./pages/signing/signing.component";
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signing' component={SigningPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signing/signing.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./signing.styles.scss"
+
+const SigningPage = () => {
+  return (
+    <div>
+      <h2>SIGN PAGE</h2>
+    </div>
+  )
+}
+
+export default SigningPage
