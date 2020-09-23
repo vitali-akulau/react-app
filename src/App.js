2bd5ecdcb7da923b7eb40912effafe420dfead0f
s34rching <ivan.katsenya@gmail.com>
2020-09-23 20:59:54 +0300

    Set to store user object

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import './App.css';
 import { Switch, Route } from 'react-router-dom'
+import './App.css';
+import { auth } from "./firebase/firebase.utils"
+import React from 'react';
 
 import Header from "./components/header/header.component";
-
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SigningPage from "./pages/signing/signing.component";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signing' component={SigningPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/shop' component={ShopPage} />
+            <Route exact path='/signing' component={SigningPage} />
+          </Switch>
+        </div>
+    );
+  }
 }
 
 export default App;
