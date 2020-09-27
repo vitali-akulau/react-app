3a09e9e08b26f7c98a7d458f0dc74b5709981672
s34rching <ivan.katsenya@gmail.com>
2020-09-27 21:45:10 +0300

    Add cart dropdown markup

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
+import Cart from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
@@ -25,6 +26,7 @@ const Header = ({ currentUser }) => (
         }
       <CartIcon />
     </div>
+    <Cart />
   </div>
 );
 
