8e7a6f3edd3ac740186285a4fe9fac90a22780cf
s34rching <ivan.katsenya@gmail.com>
2020-09-29 00:17:17 +0300

    Stop eslint barking on shadow var

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ item, addItem }) => {
+const CollectionItem = ({ item, addItemToCart }) => {
   const { imageUrl, name, price } = item;
 
   return (
@@ -17,13 +17,13 @@ const CollectionItem = ({ item, addItem }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>Add to Cart</CustomButton>
+      <CustomButton onClick={() => addItemToCart(item)} inverted>Add to Cart</CustomButton>
     </div>
   );
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  addItem: (item) => dispatch(addItem(item)),
+  addItemToCart: (item) => dispatch(addItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
