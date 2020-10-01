d82fbd8f91f783950a5c200cf7636bb9bff27a35
s34rching <ivan.katsenya@gmail.com>
2020-10-01 11:41:28 +0300

    Add util function to increase quantity on added items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartTypes from "./cart.types";
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartTypes.ADD_TO_CART:
       return {
         ...state,
-        cartItems: [ ...state.cartItems, action.payload ]
+        cartItems: addItemToCart(state.cartItems, action.payload),
       }
     default:
       return state;
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+import * as _ from 'lodash';
+
+export const addItemToCart = (cartItems, itemToAdd) => {
+  const existingItem = _.find(cartItems, { id: itemToAdd.id });
+
+  if (existingItem) {
+    return _.map(cartItems, item => {
+      return (item.id === itemToAdd.id)
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    });
+  }
+
+  return [ ...cartItems, { ...itemToAdd, quantity: 1 } ]
+};
