6d14cea6aeebf55bb88b27e299774308c935edcc
s34rching <ivan.katsenya@gmail.com>
2020-10-05 00:26:49 +0300

    Add elements to 'checkout' page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,6 +12,11 @@ export const selectCartItemsCount = createSelector(
   (cartItems) => cartItems.reduce((count, item) => count + item.quantity, 0),
 );
 
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce((count, item) => count + item.quantity * item.price, 0),
+)
+
 export const selectCartHidden = createSelector(
   [selectCart],
   (cart) => cart.hidden,
