fa9c4fd7e33890a6be74e81cf831eb1d1d055e09
s34rching <ivan.katsenya@gmail.com>
2020-10-05 10:28:09 +0300

    Dispatch toggleCartDropdown shorthand

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,8 +6,9 @@ import { withRouter } from 'react-router-dom';
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartDropdown } from "../../redux/cart/cart.actions"
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {
@@ -16,7 +17,13 @@ const CartDropdown = ({ cartItems, history }) => (
           : <span className="empty-message">Your cart is empty</span>
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => {
+        history.push('/checkout')
+        dispatch(toggleCartDropdown())
+      }
+    }>
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
