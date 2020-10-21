4cf0d87759eec5af8e7ba3a759c4daf6b4ca3121
s34rching <ivan.katsenya@gmail.com>
2020-10-21 21:26:45 +0300

    Update 'collection-item' component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import './collection-item.styles.scss';
 import { connect } from 'react-redux';
+import {
+  CollectionItemContainer,
+  CollectionItemImageContainer,
+  CollectionFooterContainer,
+  CollectionItemNameContainer,
+  CollectionItemPriceContainer,
+} from './collection-item.styles';
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
 const CollectionItem = ({ item, addItemToCart }) => {
-  const { imageUrl, name, price } = item;
+  const { name, price } = item;
 
   return (
-    <div className="collection-item">
-      <div
-        className="image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+    <CollectionItemContainer>
+      <CollectionItemImageContainer {...item} />
+      <CollectionFooterContainer>
+        <CollectionItemNameContainer>{name}</CollectionItemNameContainer>
+        <CollectionItemPriceContainer>{price}</CollectionItemPriceContainer>
+      </CollectionFooterContainer>
       <CustomButton onClick={() => addItemToCart(item)} inverted>Add to Cart</CustomButton>
-    </div>
+    </CollectionItemContainer>
   );
 };
 
