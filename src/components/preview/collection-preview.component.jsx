7612b845aa69621063da018db600137d4fec19fc
s34rching <ivan.katsenya@gmail.com>
2020-10-22 08:42:38 +0300

    Update 'collection-preview' component

--- src/components/preview/collection-preview.component.jsx
+++ src/components/preview/collection-preview.component.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import './collection-preview.styles.scss';
+import { CollectionPreviewContainer, CollectionPreviewTitle, PreviewContainer } from './collection-preview.styles';
 import _ from 'lodash';
 import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items, previewItemsNumber }) => (
-  <div className="collection-preview">
-    <h1 className="title">{_.toUpper(title)}</h1>
-    <div className="preview">
+  <CollectionPreviewContainer>
+    <CollectionPreviewTitle>{_.toUpper(title)}</CollectionPreviewTitle>
+    <PreviewContainer>
       {
         _.take(items, previewItemsNumber).map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))
       }
-    </div>
-  </div>
+    </PreviewContainer>
+  </CollectionPreviewContainer>
 );
 
 export default CollectionPreview;
