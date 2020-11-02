aade49a1f8b52e6337698466dc89fa09076e8e5c
s34rching <ivan.katsenya@gmail.com>
2020-11-02 18:32:57 +0300

    Refactor user sagas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import React from 'react';
-
 import { connect } from 'react-redux'
-import setCurrentUser from "./redux/user/user.actions"
+import { selectCurrentUser} from "./redux/user/user.selectors";
 
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
@@ -12,15 +10,12 @@ import ShopPage from "./pages/shop/shop.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 
 import SigningPage from "./pages/signing/signing.component";
-import { selectCurrentUser } from "./redux/user/user.selectors";
 import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
-
     // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
     //   if (userAuth) {
     //     const userRef = await createUserProfileDocument(userAuth)
@@ -62,8 +57,4 @@ const mapStateToProps = createStructuredSelector ({
   currentUser: selectCurrentUser,
 })
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,29 +1,35 @@
 import {
   takeLatest, put, all, call,
 } from 'redux-saga/effects';
-import { googleSignInSuccess, googleSignInFailure, emailSignInSuccess } from './user.actions';
+import { signInSuccess, signInFailure } from './user.actions';
 import { googleProvider, auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 import UserActionTypes from './user.types';
+
+export function* getSnapshotFromUserAuth(userAuth) {
+  try {
+    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userSnapshot = yield userRef.get();
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
 
 export function* signInWithGoogle() {
   try {
     const { user } = yield auth.signInWithPopup(googleProvider);
-    const userRef = yield call(createUserProfileDocument, user);
-    const userSnapshot = yield userRef.get();
-    yield put(googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
-    yield put(googleSignInFailure(error));
+    yield put(signInFailure(error));
   }
 }
 
 export function* signInWithEmail({ payload: { email, password } }) {
   try {
     const { user } = yield auth.signInWithEmailAndPassword(email, password);
-    const userRef = yield call(createUserProfileDocument, user);
-    const userSnapshot = yield userRef.get();
-    yield put(emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
-    yield put(googleSignInFailure(error));
+    yield put(signInFailure(error));
   }
 }
 
