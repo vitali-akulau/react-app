fec04702d12675b2bf92042d7f397deea561f4c7
s34rching <ivan.katsenya@gmail.com>
2020-11-10 23:30:39 +0300

    Add 'compression' for gzipping files on heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,6 +11,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,22 +20,22 @@ app.use(cors());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function(req, res) {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index/html'));
   });
 }
 
-app.listen(port, error => {
+app.listen(port, (error) => {
   if (error) throw error;
   console.log(`Server is running on port ${port}`);
-})
+});
 
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
     currency: 'usd',
-  }
+  };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
@@ -41,5 +43,5 @@ app.post('/payment', (req, res) => {
     } else {
       res.status(200).send({ success: stripeRes });
     }
-  })
-})
+  });
+});
