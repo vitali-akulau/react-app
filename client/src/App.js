d05574d55d84f054c5ddc5fd3b719fe26b2fe85b
s34rching <ivan.katsenya@gmail.com>
2020-11-11 19:31:23 +0300

    Enable service-worker

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Header from './components/header/header.component';
 import Spinner from './components/spinner/spinner.component';
 import ErrorBoundary from './components/error-boundary/error-boundary.component';
 
+import * as serviceWorker from './serviceWorker';
+
 const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
 const ShopPage = lazy(() => import('./pages/shop/shop.component'));
 const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
@@ -49,5 +51,7 @@ const mapStateToProps = createStructuredSelector({
 const mapDispatchToProps = (dispatch) => ({
   checkUserSession: () => dispatch(checkUserSession()),
 });
+
+serviceWorker.register();
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
