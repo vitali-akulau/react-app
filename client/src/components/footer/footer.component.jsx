c9a78fccf6ed134f42cd62f8eee6d2fbbad3df24
s34rching <ivan.katsenya@gmail.com>
2020-11-14 00:05:37 +0300

    Remove logo from footer

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ReactComponent as Logo } from '../../static/crown.svg';
 import { ReactComponent as TiktokLogo } from '../../static/tiktok.svg';
 import { ReactComponent as InstagramLogo } from '../../static/instagram.svg';
 import { ReactComponent as TwitterLogo } from '../../static/twitter.svg';
@@ -8,37 +7,33 @@ import {
   FooterContainer,
   FooterColumnsContainer,
   LinksColumnContainer,
-  FooterImageContainer,
   FooterLink,
   FooterExternalLink,
-  HalfSizeContainer,
 } from './footer.styles';
 
 const Footer = () => (
   <FooterContainer>
     <FooterColumnsContainer>
       <LinksColumnContainer>
-        <HalfSizeContainer>
-          <h4>COMPANY</h4>
-          <ul>
-            <li><FooterLink to="/">Careers</FooterLink></li>
-            <li><FooterLink to="/contact">Contact Us</FooterLink></li>
-            <li><FooterLink to="/">Terms</FooterLink></li>
-            <li><FooterLink to="/">Privacy</FooterLink></li>
-            <li><FooterLink to="/">Accessibility Policy</FooterLink></li>
-          </ul>
-        </HalfSizeContainer>
-        <HalfSizeContainer>
-          <h4>LEARN MORE</h4>
-          <ul>
-            <li><FooterExternalLink href="https://reactjs.org/">React</FooterExternalLink></li>
-            <li><FooterExternalLink href="https://redux.js.org/">Redux</FooterExternalLink></li>
-            <li><FooterExternalLink href="https://expressjs.com/">Express</FooterExternalLink></li>
-            <li><FooterExternalLink href="https://firebase.google.com/">Firebase</FooterExternalLink></li>
-            <li><FooterExternalLink href="https://stripe.com/">Stripe</FooterExternalLink></li>
-            <li><FooterExternalLink href="https://jestjs.io/">Jest</FooterExternalLink></li>
-          </ul>
-        </HalfSizeContainer>
+        <h4>COMPANY</h4>
+        <ul>
+          <li><FooterLink to="/">Careers</FooterLink></li>
+          <li><FooterLink to="/contact">Contact Us</FooterLink></li>
+          <li><FooterLink to="/">Terms</FooterLink></li>
+          <li><FooterLink to="/">Privacy</FooterLink></li>
+          <li><FooterLink to="/">Accessibility Policy</FooterLink></li>
+        </ul>
+      </LinksColumnContainer>
+      <LinksColumnContainer>
+        <h4>LEARN MORE</h4>
+        <ul>
+          <li><FooterExternalLink href="https://reactjs.org/">React</FooterExternalLink></li>
+          <li><FooterExternalLink href="https://redux.js.org/">Redux</FooterExternalLink></li>
+          <li><FooterExternalLink href="https://expressjs.com/">Express</FooterExternalLink></li>
+          <li><FooterExternalLink href="https://firebase.google.com/">Firebase</FooterExternalLink></li>
+          <li><FooterExternalLink href="https://stripe.com/">Stripe</FooterExternalLink></li>
+          <li><FooterExternalLink href="https://jestjs.io/">Jest</FooterExternalLink></li>
+        </ul>
       </LinksColumnContainer>
       <LinksColumnContainer>
         <ul>
@@ -48,12 +43,7 @@ const Footer = () => (
           <li><FooterLink to="/"><h4>SEND US FEEDBACK</h4></FooterLink></li>
         </ul>
       </LinksColumnContainer>
-      <LinksColumnContainer>
-        <FooterImageContainer to="/">
-          <Logo width="125px" height="125px" />
-        </FooterImageContainer>
-      </LinksColumnContainer>
-      <LinksColumnContainer textAlign="right">
+      <LinksColumnContainer textAlign="right" blocks={3} float="right">
         <h4>JOIN US</h4>
         <FooterExternalLink href="https://www.instagram.com/"><InstagramLogo /></FooterExternalLink>
         <FooterExternalLink href="https://www.tiktok.com/en/"><TiktokLogo /></FooterExternalLink>
--- client/src/components/footer/footer.styles.jsx
+++ client/src/components/footer/footer.styles.jsx
@@ -10,25 +10,19 @@ export const FooterContainer = styled.div`
   height: 200px;
   padding: 0px 87.5px 20px;
   border-top: 0.5px solid #e3e1df;
-  display: flex;
-  justify-content: space-around;
 `;
 
 export const FooterColumnsContainer = styled.div`
   width: 100%;
-  display: grid;
-  grid-column-gap: 10px;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-`;
-
-export const HalfSizeContainer = styled.div`
-  width: 50%;
-  float: left;
 `;
 
 export const LinksColumnContainer = styled.div`
+  width: ${({ blocks }) => `${blocks ? parseInt(blocks, 10) * 16 : 16}%`};
   display: inline-block;
   text-align: ${({ textAlign }) => textAlign};
+  display: inline-block;
+  vertical-align: top;
+  float: ${({ float }) => float};
 
   & ul {
     padding: unset;
@@ -38,15 +32,6 @@ export const LinksColumnContainer = styled.div`
     list-style: none;
     margin: 2px 0px;
   }
-`;
-
-export const FooterImageContainer = styled(Link)`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
 
 export const FooterLink = styled(Link)`
