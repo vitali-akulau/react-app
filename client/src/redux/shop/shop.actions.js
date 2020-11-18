b33fd37b2f3eec674d1e2c8eddf50d4429c7a429
s34rching <ivan.katsenya@gmail.com>
2020-11-18 23:10:11 +0300

    Remove unused redux-thunk

--- client/src/redux/shop/shop.actions.js
+++ client/src/redux/shop/shop.actions.js
@@ -1,5 +1,4 @@
 import ShopTypes from './shop.types';
-import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
 
 export const fetchCollectionsStart = () => ({
   type: ShopTypes.FETCH_COLLECTIONS_START,
@@ -14,17 +13,3 @@ export const fetchCollectionsFailure = (errorMessage) => ({
   type: ShopTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage,
 });
-
-export const fetchCollectionsAsync = () => (dispatch) => {
-  const collectionRef = firestore.collection('collections');
-  dispatch(fetchCollectionsStart());
-
-  collectionRef.get()
-    .then(async (snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(fetchCollectionsSuccess(collectionsMap));
-    })
-    .catch((error) => {
-      dispatch(fetchCollectionsFailure(error.message));
-    });
-};
