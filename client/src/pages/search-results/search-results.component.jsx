64cfaa6a3fb7dbd9fc909862f14b78fceb0d708c
s34rching <ivan.katsenya@gmail.com>
2020-11-22 19:13:28 +0300

    Add empty SRP block

--- client/src/pages/search-results/search-results.component.jsx
+++ client/src/pages/search-results/search-results.component.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import * as _ from 'lodash';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
+import { EmptyResultsContainer, SearchTitle } from './search-results.styles';
 import CollectionPreview from '../../components/preview/collection-preview.component';
-import { selectQueriedProducts, selectIsFetching } from '../../redux/search/search.selectors';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import { selectQueriedProducts, selectIsFetching } from '../../redux/search/search.selectors';
 
 const SearchResultsPage = ({ isLoading, products }) => (
-  products
-    ? (
-      <CollectionPreview
-        title="Search"
-        items={products}
-        previewItemsNumber={products.length}
-      />
-    ) : null
+  <div>
+    <SearchTitle>SEARCH</SearchTitle>
+    {
+        (products === null || (products.length === 0 && _.isArray(products)))
+          ? (
+            <EmptyResultsContainer>
+              <p>Nothing found...</p>
+            </EmptyResultsContainer>
+          ) : (
+            <CollectionPreview
+              items={products}
+              previewItemsNumber={products.length}
+            />
+          )
+      }
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
---
+++ client/src/pages/search-results/search-results.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const EmptyResultsContainer = styled.div`
+  font-size: 24px;
+`;
+
+export const SearchTitle = styled.h2`
+  font-size: 28px;
+`;
