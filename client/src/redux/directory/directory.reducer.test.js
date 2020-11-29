525a519380dc398d00919ba12204a5ddb02c4f3b
s34rching <ivan.katsenya@gmail.com>
2020-11-30 00:45:53 +0300

    Add test for directory reducer

---
+++ client/src/redux/directory/directory.reducer.test.js
@@ -0,0 +1,13 @@
+import directoryReducer, { INITIAL_STATE } from './directory.reducer';
+
+describe('Redux: Directory Reducer', () => {
+  it('should return initial state', () => {
+    expect(directoryReducer(undefined, {})).toEqual(INITIAL_STATE);
+  });
+
+  it('should return initial state if unrecognized action provided', () => {
+    const unrecognizedAction = { type: 'DO_SOMETHING', payload: 'payload' };
+
+    expect(directoryReducer(undefined, unrecognizedAction)).toEqual(INITIAL_STATE);
+  });
+});
