3933daac56ca61f0b8ee43ef7b4245c40ee41b5d
s34rching <ivan.katsenya@gmail.com>
2020-12-01 17:44:22 +0300

    Add test to search actions

---
+++ client/src/redux/search/search.actions.test.js
@@ -0,0 +1,25 @@
+import * as cartActions from './search.actions';
+import SearchTypes from './search.types';
+
+describe('Redux: Search Actions', () => {
+  it('"searchProductsStart" should create action to start search', () => {
+    const searchQuery = 'reebok';
+
+    expect(cartActions.searchProductsStart(searchQuery))
+      .toEqual({ type: SearchTypes.SEARCH_PRODUCTS_START, payload: searchQuery });
+  });
+
+  it('"searchProductsSuccess" should create action to successful search', () => {
+    const products = [{ id: 1, name: 'name1' }, { id: 2, name: 'name2' }];
+
+    expect(cartActions.searchProductsSuccess(products))
+      .toEqual({ type: SearchTypes.SEARCH_PRODUCTS_SUCCESS, payload: products });
+  });
+
+  it('"SearchTypes" should create action to failed search', () => {
+    const error = 'error message';
+
+    expect(cartActions.searchProductsFailure(error))
+      .toEqual({ type: SearchTypes.SEARCH_PRODUCTS_FAILURE, payload: error });
+  });
+});
