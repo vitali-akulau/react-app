457f45eccf0732bdccf5692ccee251f6d2d2b402
s34rching <ivan.katsenya@gmail.com>
2020-12-02 23:09:32 +0300

    Add test to directory selectors

--- client/src/redux/directory/directory.selectors.js
+++ client/src/redux/directory/directory.selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 
-const selectDirectory = (state) => state.directory;
+export const selectDirectory = (state) => state.directory;
 
 export const selectDirectorySections = createSelector(
   [selectDirectory],
--- client/src/redux/directory/directory.selectors.test.js
+++ client/src/redux/directory/directory.selectors.test.js
@@ -1,1 +1,20 @@
-import
+import { selectDirectory, selectDirectorySections } from './directory.selectors';
+
+describe('Redux: Directory Selectors', () => {
+  const mockState = {
+    directory: {
+      sections: [
+        { name: 'section_name_1' },
+        { name: 'section_name_2' },
+      ],
+    },
+  };
+
+  it('"selectDirectory" should return directory', () => {
+    expect(selectDirectory(mockState)).toEqual(mockState.directory);
+  });
+
+  it('"selectDirectorySections" should return directory sections', () => {
+    expect(selectDirectorySections(mockState)).toEqual(mockState.directory.sections);
+  });
+});
