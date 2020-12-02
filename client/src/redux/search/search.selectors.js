dbfdb09ccf19df7ecd115b23e8cd1e7966fb436b
s34rching <ivan.katsenya@gmail.com>
2020-12-02 23:19:17 +0300

    Add tests to search selectors

--- client/src/redux/search/search.selectors.js
+++ client/src/redux/search/search.selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 
-const selectSearch = (state) => state.search;
+export const selectSearch = (state) => state.search;
 
 export const selectQueriedProducts = createSelector(
   [selectSearch],
