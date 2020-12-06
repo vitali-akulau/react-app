31d1bacf4935ab8ba03122dee5c9f073f654a84f
s34rching <ivan.katsenya@gmail.com>
2020-12-06 23:21:43 +0300

    Add test to search saga / Draft

---
+++ client/src/redux/search/search.sagas.test.js
@@ -0,0 +1,19 @@
+import { runSaga } from 'redux-saga';
+import { takeLatest } from 'redux-saga/effects';
+import { searchProductsStart, searchProductsStartAsync } from './search.sagas';
+import SearchTypes from './search.types';
+
+describe('Redux: Search Sagas', () => {
+  describe('searchProductsStart', () => {
+    const genObject = searchProductsStart();
+
+    it('should take latest "SEARCH_PRODUCTS_START" action and call "searchProductsStartAsync"', () => {
+      expect(genObject.next().value)
+        .toEqual(takeLatest(SearchTypes.SEARCH_PRODUCTS_START, searchProductsStartAsync));
+    });
+
+    it('should be done on next iteration', () => {
+      expect(genObject.next().done).toBeTruthy();
+    });
+  });
+});
