a6fa00ec8bee65022fec60b720c2ebf5431dae9f
s34rching <ivan.katsenya@gmail.com>
2020-12-06 22:57:58 +0300

    Add tests to cart sagas

---
+++ client/src/redux/cart/cart.sagas.test.js
@@ -0,0 +1,33 @@
+import { takeLatest } from 'redux-saga/effects';
+import { runSaga } from 'redux-saga';
+import { onSignOutSuccess, clearCartOnSignOut } from './cart.sagas';
+import * as Actions from './cart.actions';
+import UserActionTypes from '../user/user.types';
+
+describe('Redux: Cart Sagas', () => {
+  describe('onSignOutSuccess', () => {
+    const genObject = onSignOutSuccess();
+
+    it('should wait for every "SIGN_OUT_SUCCESS" action and call "clearCartOnSignOut"', () => {
+      expect(genObject.next().value)
+        .toEqual(takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut));
+    });
+
+    it('should be done on next iteration', () => {
+      expect(genObject.next().done).toBeTruthy();
+    });
+  });
+
+  describe('clearCartOnSignOut', () => {
+    it('should call "clearCart" action', async () => {
+      const mockClearCart = jest.spyOn(Actions, 'clearCart');
+      const dispatched = [];
+
+      await runSaga({
+        dispatch: (action) => dispatched.push(action),
+      }, clearCartOnSignOut);
+
+      expect(mockClearCart).toHaveBeenCalledTimes(1);
+    });
+  });
+});
