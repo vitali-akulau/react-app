bfd44bf2cdcfbe7d52668441d11d0da943a94fa6
s34rching <ivan.katsenya@gmail.com>
2020-12-10 16:29:59 +0300

    Create search utils

--- client/src/redux/search/search.sagas.js
+++ client/src/redux/search/search.sagas.js
@@ -1,18 +1,9 @@
 import {
   takeLatest, call, put, all,
 } from 'redux-saga/effects';
-import * as _ from 'lodash';
 import SearchTypes from './search.types';
 import { searchProductsSuccess, searchProductsFailure } from './search.actions';
 import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
-
-const getProductsBySearchQuery = (collections, searchQuery) => {
-  const allProducts = _.flatten(_.map(collections, 'items'));
-
-  return _.filter(allProducts, ({ name }) => (
-    _.toLower(name).includes(_.toLower(searchQuery))
-  ));
-};
 
 export function* searchProductsStartAsync({ payload: searchQuery }) {
   try {
