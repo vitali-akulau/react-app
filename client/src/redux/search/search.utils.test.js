b46ff69c774fc22138c50378de0d4d2d7b49cfc8
s34rching <ivan.katsenya@gmail.com>
2020-12-12 01:00:09 +0300

    Add test to search utils

---
+++ client/src/redux/search/search.utils.test.js
@@ -0,0 +1,76 @@
+import { getProductsBySearchQuery } from './search.utils';
+import * as firebaseUtils from '../../firebase/firebase.utils';
+
+describe('Redux: Search Utils', () => {
+  describe('getProductsBySearchQuery', () => {
+    const products = {
+      valid: {
+        exact: { id: 123, name: 'jacket' },
+        upperCased: { id: 122, name: 'JACKET' },
+        capitalized: { id: 124, name: "Jacket Levi's" },
+        twoWords: { id: 125, name: 'black jacket' },
+      },
+      invalid: {
+        different: { id: 12, name: 'boots' },
+      },
+    };
+    const collectionMap = [
+      {
+        id: 1,
+        title: 'collection_1',
+        items: [products.valid.capitalized, products.valid.upperCased, products.invalid.different],
+      },
+      {
+        id: 2,
+        title: 'collection_2',
+        items: [products.valid.exact, products.valid.twoWords],
+      },
+    ];
+    const snapshot = {
+      portion: '1', of: '2', complex: '3', data: '4',
+    };
+    const convertCollectionsSnapshotToMapMock = jest.spyOn(firebaseUtils, 'convertCollectionsSnapshotToMap')
+      .mockImplementation(() => collectionMap);
+    const searchQuery = 'jacket';
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should call data handler', () => {
+      getProductsBySearchQuery(snapshot, searchQuery);
+      expect(convertCollectionsSnapshotToMapMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('should pass proper data to handler', () => {
+      getProductsBySearchQuery(snapshot, searchQuery);
+      expect(convertCollectionsSnapshotToMapMock).toHaveBeenCalledWith(snapshot);
+    });
+
+    it('should search for valid products', () => {
+      expect(getProductsBySearchQuery(snapshot, searchQuery))
+        .toIncludeAllMembers(Object.values(products.valid));
+    });
+
+    it('should ignore query case', () => {
+      const query = 'JACKET';
+
+      expect(getProductsBySearchQuery(snapshot, query))
+        .toIncludeAllMembers(Object.values(products.valid));
+    });
+
+    it('should search for valid products if query has two words', () => {
+      const query = 'black jacket';
+
+      expect(getProductsBySearchQuery(snapshot, query))
+        .toIncludeAllMembers([products.valid.twoWords]);
+    });
+
+    it('should return empty array if nothing is found', () => {
+      const query = 'flowers wallpapers';
+
+      expect(getProductsBySearchQuery(snapshot, query))
+        .toBeEmpty();
+    });
+  });
+});
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -5,5 +5,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-extended';
 
 configure({ adapter: new Adapter() });
