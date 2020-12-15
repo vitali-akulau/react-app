bb025a6997646d2d8b3a1a94e3d4f6782c1f9bc7
s34rching <ivan.katsenya@gmail.com>
2020-12-15 19:53:35 +0300

    Update search action tests

--- client/src/redux/search/search.actions.test.js
+++ client/src/redux/search/search.actions.test.js
@@ -4,12 +4,9 @@ import SearchTypes from './search.types';
 
 describe('Redux: Search Actions', () => {
   const searchQuery = 'reebok';
-  const products = [{ id: 1, name: 'name1' }, { id: 2, name: 'name2' }];
   const snapshot = {
     portion: '1', of: '2', complex: '3', data: '4',
   };
-  const getProductsBySearchQueryMock = jest.spyOn(searchUtils, 'getProductsBySearchQuery')
-    .mockImplementation(() => (products));
 
   afterEach(() => {
     jest.clearAllMocks();
@@ -20,19 +17,9 @@ describe('Redux: Search Actions', () => {
       .toEqual({ type: SearchTypes.SEARCH_PRODUCTS_START, payload: searchQuery });
   });
 
-  it('"searchProductsSuccess" should call data handler', () => {
-    cartActions.searchProductsSuccess(snapshot, searchQuery);
-    expect(getProductsBySearchQueryMock).toHaveBeenCalledTimes(1);
-  });
-
-  it('"searchProductsSuccess" should pass proper data to handler', () => {
-    cartActions.searchProductsSuccess(snapshot, searchQuery);
-    expect(getProductsBySearchQueryMock).toHaveBeenCalledWith(snapshot, searchQuery);
-  });
-
   it('"searchProductsSuccess" should create action to successful search', () => {
-    expect(cartActions.searchProductsSuccess(snapshot, searchQuery))
-      .toEqual({ type: SearchTypes.SEARCH_PRODUCTS_SUCCESS, payload: products });
+    expect(cartActions.searchProductsSuccess(snapshot))
+      .toEqual({ type: SearchTypes.SEARCH_PRODUCTS_SUCCESS, payload: snapshot });
   });
 
   it('"searchProductsFailure" should create action to failed search', () => {
