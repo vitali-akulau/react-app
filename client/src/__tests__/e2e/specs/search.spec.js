45a47fad91c4da281b5368e9cfd6e49276d3c6b0
s34rching <ivan.katsenya@gmail.com>
2021-02-28 22:50:35 +0300

    Rework 'search' spec

--- client/src/__tests__/e2e/specs/search.spec.js
+++ client/src/__tests__/e2e/specs/search.spec.js
@@ -7,20 +7,21 @@ const { filterProductsByNameSubstring, getProductNameSubstring } = require('../s
 const chance = new Chance();
 
 describe('Search', () => {
-  it('TA-36: User can search for products by exact product name', () => {
-    const product = getRandomProduct();
+  const product = getRandomProduct();
 
+  beforeEach(() => {
     Homepage.open('/');
+  });
+
+  it('TA-36: User can search for products by exact product name', () => {
     Homepage.searchForProduct(product.name);
     expect(SearchResultsPage.getFoundProductById(product.id)).toBeDisplayed();
   });
 
   it('TA-36.1: User can search for products by product name substring', () => {
-    const product = getRandomProduct();
     const query = getProductNameSubstring(product.name);
     const expectedProducts = filterProductsByNameSubstring(query);
 
-    Homepage.open('/');
     Homepage.searchForProduct(query);
     expect(SearchResultsPage.getAllFoundProducts(expectedProducts.length))
       .toHaveTextContaining(query);
@@ -29,13 +30,11 @@ describe('Search', () => {
   it('TA-37: Empty SERP is displayed when user submits invalid search query', () => {
     const query = chance.sentence();
 
-    Homepage.open('/');
     Homepage.searchForProduct(query);
     expect(SearchResultsPage.emptyResultsContainer).toBeDisplayed();
   });
 
   it('TA-38.1: User should not be able to submit empty query', () => {
-    Homepage.open('/');
     Homepage.searchForProduct('');
     expect(browser.url).not.toContain('search?q');
   });
