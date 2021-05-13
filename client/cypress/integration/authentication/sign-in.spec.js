9934e112be04e1a04b8a04ac084569bd95b3215a
Vitali Akulau <jerimi2008@tut.by>
2021-05-14 00:45:51 +0300

    update asserts in the tests

--- client/cypress/integration/authentication/sign-in.spec.js
+++ client/cypress/integration/authentication/sign-in.spec.js
@@ -6,7 +6,7 @@ import userData from '../../data/userData';
 const SIGN_OUT_LINK_TEXT = 'SIGN OUT';
 const SIGN_IN_LINK_TEXT = 'SIGN IN';
 const WARNING_TEXT = 'The email address is already in use by another account.';
-const URL_PATH = 'http://127.0.0.1:3000/';
+const HOME_PAGE_URL_PATH = 'http://127.0.0.1:3000/';
 let currentUser;
 
 describe('Sign in page', () => {
@@ -23,12 +23,10 @@ describe('Sign in page', () => {
       .fillSignInForm(currentUser.email, currentUser.password)
       .clickSignInButton();
 
-    cy.get(HomePage.signOutLink)
-      .should('be.visible')
-      .and('contain.text', SIGN_OUT_LINK_TEXT);
+    assertUserLogged();
   });
 
-  it.skip('User should not be able to login use wrong email ', () => {
+  it('User should not be able to login use wrong email ', () => {
     currentUser = user.createUser(
       userData.name, userData.wrongEmail, userData.password, userData.confirmPassword,
     );
@@ -37,12 +35,10 @@ describe('Sign in page', () => {
       .fillSignInForm(currentUser.email, currentUser.password)
       .clickSignInButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signInButton)
-      .should('be.visible');
+    assertSignInFormIsDisplay();
   });
 
-  it.skip('User should not be able to login use empty email', () => {
+  it('User should not be able to login use empty email', () => {
     currentUser = user.createUser(
       userData.name, userData.emptyEmail, userData.password, userData.confirmPassword,
     );
@@ -51,12 +47,10 @@ describe('Sign in page', () => {
       .fillSignInForm(currentUser.email, currentUser.password)
       .clickSignInButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signInButton)
-      .should('be.visible');
+    assertSignInFormIsDisplay();
   });
 
-  it.skip('User should not be able to login use wrong password', () => {
+  it('User should not be able to login use wrong password', () => {
     currentUser = user.createUser(
       userData.name, userData.emptyEmail, userData.wrongPassword, userData.confirmPassword,
     );
@@ -65,12 +59,10 @@ describe('Sign in page', () => {
       .fillSignInForm(currentUser.email, currentUser.password)
       .clickSignInButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signInButton)
-      .should('be.visible');
+    assertSignInFormIsDisplay();
   });
 
-  it.skip('User should not be able to login use empty password', () => {
+  it('User should not be able to login use empty password', () => {
     currentUser = user.createUser(
       userData.name, userData.emptyEmail, userData.emptyPassword, userData.confirmPassword,
     );
@@ -79,12 +71,10 @@ describe('Sign in page', () => {
       .fillSignInForm(currentUser.email, currentUser.password)
       .clickSignInButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signInButton)
-      .should('be.visible');
+    assertSignInFormIsDisplay();
   });
 
-  it.skip('User should not be able to login as unregistered userData', () => {
+  it('User should not be able to login as unregistered userData', () => {
     currentUser = user.createUser(
       userData.name, userData.emptyEmail, userData.password, userData.confirmPassword,
     );
@@ -93,12 +83,10 @@ describe('Sign in page', () => {
       .fillSignInForm(currentUser.unregisteredEmail, currentUser.password)
       .clickSignInButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signInButton)
-      .should('be.visible');
+    assertSignInFormIsDisplay();
   });
 
-  it.skip('User should be able to sign up use valid credentials', () => {
+  it('User should be able to sign up use valid credentials', () => {
     currentUser = user.createUser(
       userData.name, userData.newEmail, userData.password, userData.confirmPassword,
     );
@@ -108,12 +96,10 @@ describe('Sign in page', () => {
         currentUser.confirmPassword)
       .clickSignUpButton();
 
-    cy.get(HomePage.signOutLink)
-      .should('be.visible')
-      .and('contain.text', SIGN_OUT_LINK_TEXT);
+    assertUserLogged();
   });
 
-  it.skip('User should not be able to sign up with missing name field', () => {
+  it('User should not be able to sign up with missing name field', () => {
     currentUser = user.createUser(
       userData.emptyName, userData.existEmail, userData.password, userData.confirmPassword,
     );
@@ -123,12 +109,10 @@ describe('Sign in page', () => {
         currentUser.confirmPassword)
       .clickSignUpButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signUpButton)
-      .should('be.visible');
+    assertSignUpFormIsDisplay();
   });
 
-  it.skip('User should not be able to sign up with missing email field', () => {
+  it('User should not be able to sign up with missing email field', () => {
     currentUser = user.createUser(
       userData.name, userData.emptyEmail, userData.password, userData.confirmPassword,
     );
@@ -138,12 +122,10 @@ describe('Sign in page', () => {
         currentUser.confirmPassword)
       .clickSignUpButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signUpButton)
-      .should('be.visible');
+    assertSignUpFormIsDisplay();
   });
 
-  it.skip('User should not be able to sign up with missing password field', () => {
+  it('User should not be able to sign up with missing password field', () => {
     currentUser = user.createUser(
       userData.name, userData.newEmail, userData.emptyPassword, userData.confirmPassword,
     );
@@ -153,12 +135,10 @@ describe('Sign in page', () => {
         currentUser.confirmPassword)
       .clickSignUpButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signUpButton)
-      .should('be.visible');
+    assertSignUpFormIsDisplay();
   });
 
-  it.skip('User should not be able to sign up with missing confirm password field', () => {
+  it('User should not be able to sign up with missing confirm password field', () => {
     currentUser = user.createUser(
       userData.name, userData.newEmail, userData.password, userData.emptyConfirmPassword,
     );
@@ -168,12 +148,10 @@ describe('Sign in page', () => {
         currentUser.confirmPassword)
       .clickSignUpButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signUpButton)
-      .should('be.visible');
+    assertSignUpFormIsDisplay();
   });
 
-  it.skip('User should not be able to sign up with mismatching confirm password field', () => {
+  it('User should not be able to sign up with mismatching confirm password field', () => {
     currentUser = user.createUser(
       userData.name, userData.newEmail, userData.password, userData.differentConfirmPassword,
     );
@@ -183,12 +161,10 @@ describe('Sign in page', () => {
         currentUser.confirmPassword)
       .clickSignUpButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signUpButton)
-      .should('be.visible');
+    assertSignUpFormIsDisplay();
   });
 
-  it.skip('User should not be able to sign up using existing email', () => {
+  it('User should not be able to sign up using existing email', () => {
     currentUser = user.createUser(
       userData.name, userData.existEmail, userData.password, userData.confirmPassword,
     );
@@ -198,15 +174,11 @@ describe('Sign in page', () => {
         currentUser.confirmPassword)
       .clickSignUpButton();
 
-    cy.url().should('include', '/signing')
-      .get(SignInPage.signUpButton)
-      .should('be.visible')
-      .get(SignInPage.warningNotification)
-      .should('be.visible')
-      .and('contain.text', WARNING_TEXT);
+    assertSignUpFormIsDisplay();
+    assertWarningNotificationDisplay();
   });
 
-  it.skip('User should be able to sign out', () => {
+  it('User should be able to sign out', () => {
     currentUser = user.createUser(
       userData.name, userData.existEmail, userData.password, userData.confirmPassword,
     );
@@ -216,9 +188,48 @@ describe('Sign in page', () => {
       .clickSignInButton();
     HomePage.clickSignOutLink();
 
-    cy.url().should('eq', URL_PATH)
+    assertUserLoggedOut();
+  });
+
+  function assertSignInFormIsDisplay() {
+    cy.url().should('include', '/signing')
+      .get(SignInPage.signInEmailField)
+      .should('be.visible')
+      .get(SignInPage.signInPasswordField)
+      .should('be.visible')
+      .get(SignInPage.signInButton)
+      .should('be.visible');
+  }
+
+  function assertSignUpFormIsDisplay() {
+    cy.url().should('include', '/signing')
+      .get(SignInPage.signUpNameField)
+      .should('be.visible')
+      .get(SignInPage.signUpEmailField)
+      .should('be.visible')
+      .get(SignInPage.signUpPasswordField)
+      .should('be.visible')
+      .get(SignInPage.signUpConfirmPasswordField)
+      .should('be.visible');
+  }
+
+  function assertUserLogged() {
+    cy.url().should('eq', HOME_PAGE_URL_PATH)
+      .get(HomePage.signOutLink)
+      .should('be.visible')
+      .and('contain.text', SIGN_OUT_LINK_TEXT);
+  }
+
+  function assertUserLoggedOut() {
+    cy.url().should('eq', HOME_PAGE_URL_PATH)
       .get(HomePage.signInHeaderLink)
       .should('be.visible')
       .and('contain.text', SIGN_IN_LINK_TEXT);
-  });
+  }
+
+  function assertWarningNotificationDisplay() {
+    cy.get(SignInPage.warningNotification)
+      .should('be.visible')
+      .and('contain.text', WARNING_TEXT);
+  }
 });
