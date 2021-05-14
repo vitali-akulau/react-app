d89bcd6a81e29838a5384f825814cf8d0b2d9aa2
Vitali Akulau <jerimi2008@tut.by>
2021-05-14 17:58:15 +0300

    separate 'Sign In' and 'Sign Up' tests

--- client/cypress/integration/authentication/sign-in.spec.js
+++ client/cypress/integration/authentication/sign-in.spec.js
@@ -8,118 +8,122 @@ describe('Sign in page', () => {
     cy.visit('/signing');
   });
 
-  it('User should be able to login with valid credentials', () => {
-    SignInPage
-      .fillSignInForm(user.email, user.password)
-      .clickSignInButton();
+  describe('Sign In part', () => {
+    it('User should be able to login with valid credentials', () => {
+      SignInPage
+        .fillSignInForm(user.email, user.password)
+        .clickSignInButton();
 
-    assertUserLogged();
+      assertUserLogged();
+    });
+
+    it('User should not be able to login use wrong email ', () => {
+      SignInPage
+        .fillSignInForm(user.wrongEmail, user.password)
+        .clickSignInButton();
+
+      assertUserNotLoggedIn();
+    });
+
+    it('User should not be able to login use empty email', () => {
+      SignInPage
+        .fillSignInForm(user.emptyEmail, user.password)
+        .clickSignInButton();
+
+      assertUserNotLoggedIn();
+    });
+
+    it('User should not be able to login use wrong password', () => {
+      SignInPage
+        .fillSignInForm(user.email, user.wrongPassword)
+        .clickSignInButton();
+
+      assertUserNotLoggedIn();
+    });
+
+    it('User should not be able to login use empty password', () => {
+      SignInPage
+        .fillSignInForm(user.email, user.emptyPassword)
+        .clickSignInButton();
+
+      assertUserNotLoggedIn();
+    });
+
+    it('User should not be able to login as unregistered user', () => {
+      SignInPage
+        .fillSignInForm(user.newEmail, user.password)
+        .clickSignInButton();
+
+      assertUserNotLoggedIn();
+    });
   });
 
-  it('User should not be able to login use wrong email ', () => {
-    SignInPage
-      .fillSignInForm(user.wrongEmail, user.password)
-      .clickSignInButton();
+  describe('Sign Up part', () => {
+    it('User should be able to sign up use valid credentials', () => {
+      SignInPage
+        .fillSignUpForm(user.name, user.newEmail, user.password, user.confirmPassword)
+        .clickSignUpButton();
 
-    assertUserNotLoggedIn();
-  });
+      assertUserLogged();
+    });
 
-  it('User should not be able to login use empty email', () => {
-    SignInPage
-      .fillSignInForm(user.emptyEmail, user.password)
-      .clickSignInButton();
+    it('User should not be able to sign up with missing name field', () => {
+      SignInPage
+        .fillSignUpForm(user.emptyName, user.email, user.password, user.confirmPassword)
+        .clickSignUpButton();
 
-    assertUserNotLoggedIn();
-  });
+      assertUserIsNotAuthorized();
+    });
 
-  it('User should not be able to login use wrong password', () => {
-    SignInPage
-      .fillSignInForm(user.email, user.wrongPassword)
-      .clickSignInButton();
+    it('User should not be able to sign up with missing email field', () => {
+      SignInPage
+        .fillSignUpForm(user.name, user.emptyEmail, user.password, user.confirmPassword)
+        .clickSignUpButton();
 
-    assertUserNotLoggedIn();
-  });
+      assertUserIsNotAuthorized();
+    });
 
-  it('User should not be able to login use empty password', () => {
-    SignInPage
-      .fillSignInForm(user.email, user.emptyPassword)
-      .clickSignInButton();
+    it('User should not be able to sign up with missing password field', () => {
+      SignInPage
+        .fillSignUpForm(user.name, user.email, user.emptyPassword, user.confirmPassword)
+        .clickSignUpButton();
 
-    assertUserNotLoggedIn();
-  });
+      assertUserIsNotAuthorized();
+    });
 
-  it('User should not be able to login as unregistered user', () => {
-    SignInPage
-      .fillSignInForm(user.newEmail, user.password)
-      .clickSignInButton();
+    it('User should not be able to sign up with missing confirm password field', () => {
+      SignInPage
+        .fillSignUpForm(user.name, user.email, user.password, user.emptyConfirmPassword)
+        .clickSignUpButton();
 
-    assertUserNotLoggedIn();
-  });
+      assertUserIsNotAuthorized();
+    });
 
-  it('User should be able to sign up use valid credentials', () => {
-    SignInPage
-      .fillSignUpForm(user.name, user.newEmail, user.password, user.confirmPassword)
-      .clickSignUpButton();
+    it('User should not be able to sign up with mismatching confirm password field', () => {
+      SignInPage
+        .fillSignUpForm(user.name, user.email, user.password, user.differentConfirmPassword)
+        .clickSignUpButton();
 
-    assertUserLogged();
-  });
+      assertUserIsNotAuthorized();
+    });
 
-  it('User should not be able to sign up with missing name field', () => {
-    SignInPage
-      .fillSignUpForm(user.emptyName, user.email, user.password, user.confirmPassword)
-      .clickSignUpButton();
+    it('User should not be able to sign up using existing email', () => {
+      SignInPage
+        .fillSignUpForm(user.name, user.email, user.password, user.confirmPassword)
+        .clickSignUpButton();
 
-    assertUserIsNotAuthorized();
-  });
+      assertUserIsNotAuthorized();
+      assertThatWarningNotificationIsDisplay();
+    });
 
-  it('User should not be able to sign up with missing email field', () => {
-    SignInPage
-      .fillSignUpForm(user.name, user.emptyEmail, user.password, user.confirmPassword)
-      .clickSignUpButton();
+    it('User should be able to sign out', () => {
+      SignInPage
+        .fillSignInForm(user.email, user.password)
+        .clickSignInButton();
+      HomePage.clickSignOutLink();
 
-    assertUserIsNotAuthorized();
-  });
-
-  it('User should not be able to sign up with missing password field', () => {
-    SignInPage
-      .fillSignUpForm(user.name, user.email, user.emptyPassword, user.confirmPassword)
-      .clickSignUpButton();
-
-    assertUserIsNotAuthorized();
-  });
-
-  it('User should not be able to sign up with missing confirm password field', () => {
-    SignInPage
-      .fillSignUpForm(user.name, user.email, user.password, user.emptyConfirmPassword)
-      .clickSignUpButton();
-
-    assertUserIsNotAuthorized();
-  });
-
-  it('User should not be able to sign up with mismatching confirm password field', () => {
-    SignInPage
-      .fillSignUpForm(user.name, user.email, user.password, user.differentConfirmPassword)
-      .clickSignUpButton();
-
-    assertUserIsNotAuthorized();
-  });
-
-  it('User should not be able to sign up using existing email', () => {
-    SignInPage
-      .fillSignUpForm(user.name, user.email, user.password, user.confirmPassword)
-      .clickSignUpButton();
-
-    assertUserIsNotAuthorized();
-    assertThatWarningNotificationIsDisplay();
-  });
-
-  it('User should be able to sign out', () => {
-    SignInPage
-      .fillSignInForm(user.email, user.password)
-      .clickSignInButton();
-    HomePage.clickSignOutLink();
-
-    assertUserLoggedOut();
+      assertUserLoggedOut();
+    });
   });
 
   function assertUserNotLoggedIn() {
