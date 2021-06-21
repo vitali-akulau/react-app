354bb2251bff3c0e4867b79ce5df30f0f644fe02
Vitali Akulau <jerimi2008@tut.by>
2021-06-21 14:42:59 +0300

    change iteration of test data in tests of 'sign-in' spec

--- client/cypress/integration/authentication/sign-in.spec.js
+++ client/cypress/integration/authentication/sign-in.spec.js
@@ -1,6 +1,6 @@
 import SignInPage from '../../model/SignInPage';
 import HomePage from '../../model/HomePage';
-import user from '../../fixtures/user';
+import users from '../../fixtures/users.json';
 
 describe('Sign in page', () => {
   beforeEach(() => {
@@ -10,13 +10,13 @@ describe('Sign in page', () => {
   describe('Sign In', () => {
     describe('Positive scenarios | User should be able to:', () => {
       it('login with valid credentials', () => {
-        SignInPage.signIn(user.email, user.password);
+        SignInPage.signIn(users.validUser[0].email, users.validUser[0].password);
 
         cy.get(HomePage.signOutLink).should('be.visible');
       });
 
       it('sign out', () => {
-        SignInPage.signIn(user.email, user.password);
+        SignInPage.signIn(users.validUser[0].email, users.validUser[0].password);
         HomePage.clickSignOutLink();
 
         cy.get(HomePage.signInHeaderLink).should('be.visible');
@@ -24,14 +24,12 @@ describe('Sign in page', () => {
     });
 
     describe('Negative scenarios | User should not be able to login use:', () => {
-      it('wrong email | empty email | wrong password | empty password | unregistered user', () => {
-        cy.fixture('sign-in-users').then((users) => {
-          users.forEach((data) => {
-            SignInPage.signIn(data.email, data.password);
+      users.signInInvalidUser.forEach((prop) => {
+        it(`${prop.description}`, () => {
+          SignInPage.signIn(prop.email, prop.password);
 
-            cy.url().should('include', '/signing');
-            cy.get(SignInPage.signInTitle).should('be.visible');
-          });
+          cy.url().should('include', '/signing');
+          cy.get(SignInPage.signInTitle).should('be.visible');
         });
       });
     });
@@ -40,23 +38,22 @@ describe('Sign in page', () => {
   describe('Sign Up', () => {
     describe('Positive scenarios | User should be able to:', () => {
       it('sign up use valid credentials', () => {
-        SignInPage.signUp(user.name, user.newEmail, user.password, user.confirmPassword);
+        SignInPage.signUp(users.validUser[0].name,
+          `test.user@test${Date.now('YYYY-MM-DDTHH:mm:ss'.replace(':', '-'))}.com`,
+          users.validUser[0].password,
+          users.validUser[0].confirmPassword);
 
         cy.get(HomePage.signOutLink).should('be.visible');
       });
     });
 
     describe('Negative scenarios | User should not be able to sign up with:', () => {
-      it('missing name field | missing email field | missing password field | ' +
-        'missing confirm password field | mismatching confirm password field | ' +
-        'using existing email', () => {
-        cy.fixture('sign-up-users').then((users) => {
-          users.forEach((data) => {
-            SignInPage.signUp(data.name, data.email, data.password, data.confirmPassword);
+      users.signUpInvalidUser.forEach((prop) => {
+        it(`${prop.description}`, () => {
+          SignInPage.signUp(prop.name, prop.email, prop.password, prop.confirmPassword);
 
-            cy.url().should('include', '/signing');
-            cy.get(SignInPage.signUpTitle).should('be.visible');
-          });
+          cy.url().should('include', '/signing');
+          cy.get(SignInPage.signUpTitle).should('be.visible');
         });
       });
     });
