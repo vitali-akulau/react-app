0897b1b441346c30f2b63ec40b72ededd32b89e9
Vitali Akulau <jerimi2008@tut.by>
2021-06-21 14:39:49 +0300

    replace of three test date files to one

--- client/cypress/fixtures/user.js
+++
@@ -1,9 +0,0 @@
-const user = {
-  name: 'user',
-  email: 'test@test.com',
-  newEmail: `test.user@test${Date.now()}.com`,
-  password: 123456,
-  confirmPassword: 123456,
-};
-
-export default user;
